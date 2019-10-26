662235f8f161a99b27e4f6b517e86b7abb3223f0
Geon George <geongeorge98@gmail.com>
2019-10-27 03:19:20 +0530

    basic home layout

---
+++ components/utils/Navigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-purple-800 px-6 py-2">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Rebuild Earth</span>
+    </div>
+    <div class="flex items-center flex-shrink-0 text-white mr-0">
+        <span class="font-light text-xl tracking-tight">Sign in</span>
+    </div>
+    </nav>
+</template>
---
+++ layouts/homeLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <Navigation></Navigation>
+      <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/utils/Navigation'
+
+export default {
+    components : {
+        Navigation
+    }
+}
+</script>
